db2073f094af981e2f82063104926452eec4b7e5
bekzod28072009 <bekhzodkeldiyorov@gmail.com>
2024-08-02 09:52:41 +0500

    add

---
+++ Taqqos.Domain/Commons/Auditable.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Taqqos.Domain.Enums;
+
+namespace Taqqos.Domain.Commons
+{
+    public class Auditable
+    {
+        public int Id { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public DateTime? UpdatedAt { get; set; }
+        public long? UpdatedBy { get; set; }
+        public long? CreatedBy { get; set; }
+        public ItemState State { get; set; } = ItemState.Created;
+    }
+}
