42719149eda885a31c199d616fff2b20fa7bf744
bekzod28072009 <bekhzodkeldiyorov@gmail.com>
2024-08-02 15:43:22 +0500

    add

---
+++ Taqqos.Domain/Entitys/Collectors/Collector.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Taqqos.Domain.Commons;
+using Taqqos.Domain.Entitys.Users;
+
+namespace Taqqos.Domain.Entitys.Collectors
+{
+    public class Collector : Auditable
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        public string Email { get; set; }
+
+        public int DateOfBirth { get; set; }
+
+        public HomeAddress HomeAddress { get; set; }
+    }
+}
---
+++ Taqqos.Domain/Entitys/Deliveries/Car.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Taqqos.Domain.Entitys.Deliveries
+{
+    public class Car
+    {
+        public string Name { get; set; }
+
+        public int NumberOfCar { get; set; }
+
+    }
+}
